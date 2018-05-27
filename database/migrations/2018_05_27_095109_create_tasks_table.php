61f5368365eb3ba2b73bc1d662f31ee204bca1e0
nadadahab <nadadahab906@gmail.com>
2018-05-27 13:26:52 +0200

    Update 2018_05_27_095109_create_tasks_table.php

--- database/migrations/2018_05_27_095109_create_tasks_table.php
+++ database/migrations/2018_05_27_095109_create_tasks_table.php
@@ -13,7 +13,7 @@ class CreateTasksTable extends Migration
      */
     public function up()
     {
-        Schema::create('_tasks', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->timestamps();
